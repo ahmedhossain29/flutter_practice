04acc0a383fc4cbb243d13b9d209235fad5041f5
AHMED <hossainahmad76@gmail.com>
2023-09-23 21:57:28 +0600

    Assignment_7

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,119 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/assignment_6.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Product {
+  final String name;
+  final double price;
+  int count = 0;
 
+  Product(this.name, this.price);
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: ProductList(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class ProductList extends StatefulWidget {
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+
+
+  List<Product> products = [
+    Product('Product A', 10.0),
+    Product('Product B', 15.0),
+    Product('Product C', 20.0),
+    Product('Product D', 20.0),
+    Product('Product E', 20.0),
+    Product('Product F', 20.0),
+    Product('Product G', 20.0),
+    Product('Product H', 20.0),
+    Product('Product I', 20.0),
+    Product('Product J', 20.0),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home"),),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(width: 5),
-                borderRadius: BorderRadius.all(Radius.circular(15),),
-              ),
-            height: 300,
-              child: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index){
-              return ListTile(
-                leading: Icon(Icons.person),
-                title: Text("Hossain Ahmed"),
-                subtitle: Text("Flutter Developer "),
-                trailing: Icon(Icons.add_call),
-              );
-            },
-            ),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CartPage(products)),
+          );
 
-            SizedBox(height: 10,),
+        },
+        child: Icon(Icons.shopping_cart),
+      ),
+      appBar: AppBar(
+        title: Text('Product List'),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (BuildContext context, int index) {
+          final product = products[index];
+          return ListTile(
+            title: Text(product.name),
+            subtitle: Text('Price: \$${product.price.toStringAsFixed(2)}'),
+            trailing: Column(
+              children: [
+                Text('Count: ${product.count}'),
+                Expanded(
+                  child: ElevatedButton(onPressed: (){
+                    product.count++;
+                    if(product.count==5){
 
-            Container(
-              height: 300,
-              decoration: BoxDecoration(border: Border.all(width: 5),
-              borderRadius: BorderRadius.all(Radius.circular(15)),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    child: Icon(Icons.person,size: 100,),
-                  ),
-                  Expanded(child: Container(
-                    child: ListView.builder(
-                      itemCount: 15,
-                      itemBuilder: (context, index){
-                        return  ListTile(
-                          leading: Icon(Icons.person),
-                          title: Text("Hossain Ahmed"),
-                          trailing: Icon(Icons.phone),
-                          subtitle: Text("Flutter Developer"),
+                      showDialog(context: context, builder: (BuildContext context){
+                        return AlertDialog(
+                          title: Text("Congratulation"),
+                          content: Text("You Buy ${product.name}"),
                         );
-                      },
-                    )
-                  ),),
-                ],
-              ),
-            ),
+                      });
+                    }
+                    setState(() {
 
-          ],
+                    });
+
+                  }, child: Text("BuyNow")),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+class CartPage extends StatelessWidget {
+  final List<Product> products;
+
+  CartPage(this.products);
+
+  int getTotalPurchases() {
+    return products.where((product) => product.count >=5).length;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart'),
+      ),
+      body: Center(
+        child: Text(
+          'Total Products Bought: ${getTotalPurchases()}',
+          style: TextStyle(fontSize: 24),
         ),
       ),
     );
   }
 }
-
