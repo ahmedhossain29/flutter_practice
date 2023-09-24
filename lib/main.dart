2238bebf05d1da37a84d94fcd61171f826a2fb15
AHMED <hossainahmad76@gmail.com>
2023-09-24 23:24:30 +0600

    Live Test 7

--- lib/main.dart
+++ lib/main.dart
@@ -4,116 +4,83 @@ void main() {
   runApp(MyApp());
 }
 
-class Product {
-  final String name;
-  final double price;
-  int count = 0;
-
-  Product(this.name, this.price);
-}
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProductList(),
+      home: HomeScreen(),
     );
   }
 }
 
-class ProductList extends StatefulWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
-  _ProductListState createState() => _ProductListState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _ProductListState extends State<ProductList> {
+class _HomeScreenState extends State<HomeScreen> {
+  int _count =0;
+  void _incrementCount() {
+      setState(() {
+      _count++;
+      _checkAndShowDialog();
+    });
+  }
+  void _decrementCount() {
+    setState(() {
+      if (_count > 0) {
+        _count--;
+        _checkAndShowDialog();
+      }
+    });
+  }
 
-
-  List<Product> products = [
-    Product('Product A', 10.0),
-    Product('Product B', 15.0),
-    Product('Product C', 20.0),
-    Product('Product D', 20.0),
-    Product('Product E', 20.0),
-    Product('Product F', 20.0),
-    Product('Product G', 20.0),
-    Product('Product H', 20.0),
-    Product('Product I', 20.0),
-    Product('Product J', 20.0),
-  ];
+  void _checkAndShowDialog() {
+    if (_count >= 5) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Button pressed $_count times."),
+            actions: <Widget>[
+              TextButton(
+                child: Text("Close"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => CartPage(products)),
-          );
+      appBar: AppBar(title: Text("Counter App"),),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
 
-        },
-        child: Icon(Icons.shopping_cart),
-      ),
-      appBar: AppBar(
-        title: Text('Product List'),
-      ),
-      body: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (BuildContext context, int index) {
-          final product = products[index];
-          return ListTile(
-            title: Text(product.name),
-            subtitle: Text('Price: \$${product.price.toStringAsFixed(2)}'),
-            trailing: Column(
-              children: [
-                Text('Count: ${product.count}'),
-                Expanded(
-                  child: ElevatedButton(onPressed: (){
-                    product.count++;
-                    if(product.count==5){
-
-                      showDialog(context: context, builder: (BuildContext context){
-                        return AlertDialog(
-                          title: Text("Congratulation"),
-                          content: Text("You Buy ${product.name}"),
-                        );
-                      });
-                    }
-                    setState(() {
-
-                    });
-
-                  }, child: Text("BuyNow")),
-                )
-              ],
-            ),
-          );
-        },
+        children: [
+          Text("Count",style: TextStyle(fontSize: 35),),
+          Text('$_count',style: TextStyle(fontSize: 100),),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: _incrementCount, child: Icon(Icons.add)),
+            SizedBox(width: 10,),
+            ElevatedButton(onPressed: _decrementCount, child: Icon(Icons.remove)),
+          ],
+        )
+        ],
       ),
     );
   }
 }
-class CartPage extends StatelessWidget {
-  final List<Product> products;
 
-  CartPage(this.products);
 
-  int getTotalPurchases() {
-    return products.where((product) => product.count >=5).length;
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Cart'),
-      ),
-      body: Center(
-        child: Text(
-          'Total Products Bought: ${getTotalPurchases()}',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
-}
+
